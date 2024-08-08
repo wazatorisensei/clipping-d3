8c8f73fdb78f27a59fe462fbaef2cb19a45412e2
Jan Yuzo Kohara <janyuzokohara@gmail.com>
2024-08-08 12:17:01 -0300

    CHANGE: data

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import { BarChart } from './components/BarChart';
 import { PieChart } from './components/PieChart';
 
 export const App = () => {
-  const [data, setData] = useState(generateRandomData(20000));
+  const [data, setData] = useState(generateRandomData(100));
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
       <PieChart data={data} width={500} height={500} />
-      <BarChart data={data} width={600} height={500} />
-      <button onClick={() => setData(generateRandomData(20000))}>
+      <BarChart data={data} width={500} height={500} />
+      <button onClick={() => setData(generateRandomData(100))}>
         Gerar Dados
       </button>
     </div>
